cffbd31fbfa49615380e27e5244ece81856f8620
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2025-02-27 20:24:12 -0500

    Update naive_turbidostat.py

--- dosing/naive_turbidostat.py
+++ dosing/naive_turbidostat.py
@@ -22,16 +22,3 @@ class NaiveTurbidostat(DosingAutomationJobContrib):
     def execute(self):
         if self.latest_od > self.target_od:
             self.execute_io_action(media_ml=1.0, waste_ml=1.0)
-
-
-if __name__ == "__main__":
-    from pioreactor.background_jobs.dosing_control import DosingController
-
-    dc = DosingController(
-        "naive_turbidostat",
-        target_od=2.0,
-        duration=1,  # check every 1 minute
-        unit="test_unit",
-        experiment="test_experiment",
-    )
-    dc.block_until_disconnected()
